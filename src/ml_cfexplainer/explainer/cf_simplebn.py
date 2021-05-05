4fca98e8417a0b6a540a6df58979c19562bffe6a
Dung Duong <tridungduong16@gmail.com>
2021-05-05 10:29:13 +1000

    fix bugs simplebn

--- src/ml_cfexplainer/explainer/cf_simplebn.py
+++ src/ml_cfexplainer/explainer/cf_simplebn.py
@@ -47,8 +47,8 @@ class CF_SimpleBN(Problem):
         xcf[2] = self.scm_model.predict(xcf[:2].reshape(1, -1))
 
         """Create the distance object"""
-        dist = Distance(self.x0, xcf, self.pred_model, con_index=self.con_index)
-
+        dist = Distance(self.x0, xcf, self.pred_model, self.dfencoder_model, con_index=self.con_index)
+        
         condist = dist.continous_dist()
         yloss = dist.logloss()
         # closs = causal_loss(self.scm_model, xcf, self.x0)
@@ -56,8 +56,7 @@ class CF_SimpleBN(Problem):
         df_store = pd.DataFrame(columns=self.col)
         df_store.loc[0] = xcf
         zcf = self.dfencoder_model.get_representation(df_store)[0]
-
-
+        
         out["F"] = anp.column_stack([yloss, condist])
         # out["G"] = 0.5 - yloss
 
